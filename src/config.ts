a8d70d85e652687c2ec2e93552ebc6b55a40b0b4
Yash Goyal <yashgo0018@gmail.com>
2024-05-20 14:16:57 +0200

    completed the opensea nft fetch api

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+import "dotenv/config";
+
+export const OPENSEA_API_KEY = process.env.OPENSEA_API_KEY || "";
--- src/services/opensea.ts
+++ src/services/opensea.ts
@@ -1,22 +1,72 @@
-export const fetchNft = async ({
-  chain = "base-sepolia",
-  contract,
-  tokenId,
-}: {
+import axios from "axios";
+import { OPENSEA_API_KEY } from "../config";
+import { formatUnits } from "viem";
+
+const openseaBaseUrl = "https://api.opensea.io/api/v2";
+
+export const fetchNft = async ({}: {
   chain?: string;
   contract: string;
   tokenId: number;
 }) => {
-  const title = "";
-  const description = "";
-  const ownerAddress = "";
-  const priceEth = 0;
-  const priceUsd = 0;
-  const saleEndsAt = new Date();
-  const canOffer = false;
-  return { title, description, priceEth, priceUsd, saleEndsAt, canOffer, ownerAddress };
+  const chain = "ethereum";
+  const contractAddress = "0x7aada103f7852c7e7da61e100d6277a3fd199b58";
+  const token_id = "696";
+
+  const openseaendpoint = `${openseaBaseUrl}/orders/${chain}/seaport/listings?asset_contract_address=${contractAddress}&token_ids=${token_id}`;
+  const nftEndpoint = `${openseaBaseUrl}/chain/${chain}/contract/${contractAddress}/nfts/${token_id}`;
+
+  const headers = { "x-api-key": OPENSEA_API_KEY };
+
+  let title = "";
+  let description = "";
+  let ownerAddress = "";
+  let priceEth = "0";
+  let priceUsd = 0;
+  let saleEndsAt = new Date();
+  let canOffer = false;
+  let image = "";
+
+  try {
+    const { data } = await axios.get(openseaendpoint, {
+      headers,
+    });
+
+    const {
+      data: { nft: nftData },
+    } = await axios.get(nftEndpoint, {
+      headers,
+    });
+
+    console.log(nftData);
+
+    console.log(data);
+
+    if (!data.orders || data.orders.length == 0) {
+    } else {
+      title = nftData.name;
+      description = nftData.description;
+      ownerAddress = nftData.owners[0].address;
+      image = nftData.opensea_url;
+      const price = data.orders[0].current_price;
+      priceEth = formatUnits(price, 18).toString();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  return {
+    title,
+    image,
+    description,
+    priceEth,
+    priceUsd,
+    saleEndsAt,
+    canOffer,
+    ownerAddress,
+  };
 };
 
 export const fetchNftListing = async () => {
   return [];
-}
+};
